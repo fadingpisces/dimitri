116f4c3500e74853b01a37e56ed0f46703bc39b0
sfr <saija.fahdrachid@viacesi.fr>
2020-04-06 14:59:33 +0200

    init

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -10,7 +10,7 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.BOT_TOKEN,
    autorun: true
 });
 bot.on('ready', function (evt) {
